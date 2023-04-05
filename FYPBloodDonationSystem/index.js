428918ef8a5236185e4d80a3dfa7cf97c9deae7c
AliMuhammadAslam <aaalimohdaslam@gmail.com>
2023-04-05 11:27:17 +0500

    no change

--- FYPBloodDonationSystem/index.js
+++ FYPBloodDonationSystem/index.js
@@ -9,7 +9,7 @@ import forgotPassword from './src/screens/forgotPassword';
 
 import Login from './src/screens/login';
 import Signup from './src/screens/signup';
-import forgotPassword from './src/screens/forgotPassword';
+//import forgotPassword from './src/screens/forgotPassword';
 
 
 AppRegistry.registerComponent(appName, () => forgotPassword);
