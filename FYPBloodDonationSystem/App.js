5049eb6ee1a9d4eb381b5a8387b8349d7cb73f93
RafaiTariq <rafaitariq.rt@gmail.com>
2023-05-26 01:10:21 +0500

    cosmetic changes

--- FYPBloodDonationSystem/App.js
+++ FYPBloodDonationSystem/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 //import TabNavigation from './src/navigation/TabNavigation';
 import AuthStack from './src/navigation/AuthStack';
-
+import { LogBox } from 'react-native';
 
 const App = () => {  
+  
 
+  // Ignore log notification by message
+  LogBox.ignoreLogs(['Warning: ...']);
+  
+  //Ignore all log notifications
+  LogBox.ignoreAllLogs();
   return (
     <NavigationContainer>
       <AuthStack/>
